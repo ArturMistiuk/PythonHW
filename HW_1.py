143efb4d514de2546d57e34edfeeadbf50479932
Artur Mistiuk <mistiuk.artur@gmail.com>
2022-11-28 01:20:16 +0100

    Update HW_1.py

--- HW_1.py
+++ HW_1.py
@@ -1,13 +1,12 @@
-length = 2.75
-width = 1.75
-area = length * width
-show = f'With width {width} and length {length} of the room, its area is equal to {area}'
-print(show)
-print(show)
-print(show)
-print(show)
-print(show)
-print(show)
-print(show)
-print(show)
-print(show)
+import math
+
+RADIUS = 6371.01
+lat1 = math.radians(50.45)
+lon1 = math.radians(30.523)
+
+lat2 = math.radians(51.5072)
+lon2 = math.radians(-0.1275)
+
+distance = 6371.01 * math.acos(math.sin(lat1) * math.sin(lat2) +
+                               math.cos(lat1) * math.cos(lat2) * math.cos(lon1 - lon2))
+print(round(lat1, 6))
